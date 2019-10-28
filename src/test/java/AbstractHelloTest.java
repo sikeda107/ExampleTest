c109208c200e848d7280e57ad4a3e64849adab1e
s.ikeda <s.ikeda107@gmail.com>
2019-10-29 00:51:57 +0900

    add: 単体テストファイルを作成

---
+++ src/test/java/AbstractHelloTest.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AbstractHelloTest {
+
+    @Test
+    public void abstractPublicSayHello() {
+        fail("not implemented yet");
+    }
+
+    @Test
+    public void abstractProtectedSayHello() {
+        fail("not implemented yet");
+    }
+
+    @Test
+    public void abstractSayHello() {
+        fail("not implemented yet");
+    }
+
+    @Test
+    public void publicSayHello() {
+        fail("not implemented yet");
+    }
+
+    @Test
+    public void protectedSayHello() {
+        fail("not implemented yet");
+    }
+
+    @Test
+    public void sayHello() {
+        fail("not implemented yet");
+    }
+}
---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloTest {
+
+    @Test
+    public void abstractPublicSayHello() {
+        fail("not implemented yet");
+    }
+
+    @Test
+    public void abstractProtectedSayHello() {
+        fail("not implemented yet");
+    }
+
+    @Test
+    public void abstractSayHello() {
+        fail("not implemented yet");
+    }
+}
