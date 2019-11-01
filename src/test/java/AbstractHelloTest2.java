f43acde41df05cd1a0826ebbe9637e7b2a29a843
s.ikeda <s.ikeda107@gmail.com>
2019-11-02 08:18:44 +0900

    add: AbstractHelloTest2 not use Mock

---
+++ src/test/java/AbstractHelloTest2.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AbstractHelloTest2 {
+
+    @Test
+    public void publicSayHello() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void protectedSayHello() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void sayHello() {
+        fail("Not yet implemented");
+    }
+}
