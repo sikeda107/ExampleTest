322d5cec64c5cdf9c156981588bf03fd1783223b
s.ikeda <s.ikeda107@gmail.com>
2019-11-02 08:14:18 +0900

    update: implemented abstractHello test code

--- src/test/java/AbstractHelloTest.java
+++ src/test/java/AbstractHelloTest.java
@@ -1,36 +1,37 @@
 import org.junit.Test;
+import org.mockito.Mockito;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class AbstractHelloTest {
 
     @Test
-    public void abstractPublicSayHello() {
-        fail("not implemented yet");
+    public void publicSayHello() throws Exception {
+        //抽象クラスのモック化
+        AbstractHello abstractHelloMock = Mockito.mock(AbstractHello.class, Mockito.CALLS_REAL_METHODS);
+        // 引数有りのテスト
+        assertEquals("Hello,Tom!!", abstractHelloMock.publicSayHello("Tom"));
+        //引数がnullのテスト
+        assertEquals("Hello!!", abstractHelloMock.publicSayHello(null));
     }
 
     @Test
-    public void abstractProtectedSayHello() {
-        fail("not implemented yet");
+    public void protectedSayHello() throws Exception {
+        //抽象クラスのモック化
+        AbstractHello abstractHelloMock = Mockito.mock(AbstractHello.class, Mockito.CALLS_REAL_METHODS);
+        // 引数有りのテスト
+        assertEquals("Hello,Bob!!", abstractHelloMock.protectedSayHello("Bob"));
+        //引数がnullのテスト
+        assertEquals("Hello!!", abstractHelloMock.protectedSayHello(null));
     }
 
     @Test
-    public void abstractSayHello() {
-        fail("not implemented yet");
-    }
-
-    @Test
-    public void publicSayHello() {
-        fail("not implemented yet");
-    }
-
-    @Test
-    public void protectedSayHello() {
-        fail("not implemented yet");
-    }
-
-    @Test
-    public void sayHello() {
-        fail("not implemented yet");
+    public void sayHello() throws Exception {
+        //抽象クラスのモック化
+        AbstractHello abstractHelloMock = Mockito.mock(AbstractHello.class, Mockito.CALLS_REAL_METHODS);
+        // 引数有りのテスト
+        assertEquals("Hello,Marry!!", abstractHelloMock.sayHello("Marry"));
+        //引数がnullのテスト
+        assertEquals("Hello!!", abstractHelloMock.sayHello(null));
     }
 }
