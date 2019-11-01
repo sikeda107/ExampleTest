14a1cf34fd76bbb519883df5da3e191d409a89b2
s.ikeda <s.ikeda107@gmail.com>
2019-11-02 08:35:01 +0900

    update: rename test method and implemented test code

--- src/test/java/AbstractHelloTest2.java
+++ src/test/java/AbstractHelloTest2.java
@@ -4,18 +4,50 @@ import static org.junit.Assert.*;
 
 public class AbstractHelloTest2 {
 
-    @Test
-    public void publicSayHello() {
-        fail("Not yet implemented");
+    /**
+     * モック用に抽象クラスを具象化
+     */
+    class MockAbstractHello extends AbstractHello{
+        @Override
+        public String abstractPublicSayHello(String name) {
+            return name;
+        }
+
+        @Override
+        protected String abstractProtectedSayHello(String name) {
+            return name;
+        }
+
+        @Override
+        String abstractSayHello(String name) {
+            return name;
+        }
     }
 
     @Test
-    public void protectedSayHello() {
-        fail("Not yet implemented");
+    public void publicSayHelloTest() throws Exception{
+        MockAbstractHello mockAbstractHello = new MockAbstractHello();
+        // 引数有りのテスト
+        assertEquals("Hello,Tom!!", mockAbstractHello.publicSayHello("Tom"));
+        //引数がnullのテスト
+        assertEquals("Hello!!", mockAbstractHello.publicSayHello(null));
     }
 
     @Test
-    public void sayHello() {
-        fail("Not yet implemented");
+    public void protectedSayHelloTest() throws Exception{
+        MockAbstractHello mockAbstractHello = new MockAbstractHello();
+        // 引数有りのテスト
+        assertEquals("Hello,Bob!!", mockAbstractHello.protectedSayHello("Bob"));
+        //引数がnullのテスト
+        assertEquals("Hello!!", mockAbstractHello.protectedSayHello(null));
+    }
+
+    @Test
+    public void sayHelloTest() throws Exception{
+        MockAbstractHello mockAbstractHello = new MockAbstractHello();
+        // 引数有りのテスト
+        assertEquals("Hello,Bob!!", mockAbstractHello.sayHello("Bob"));
+        //引数がnullのテスト
+        assertEquals("Hello!!", mockAbstractHello.sayHello(null));
     }
 }
